abad8484717998d4a926f9f2383e5e7dd3e67b19
saibabug <saibabug@users.noreply.github.com>
2020-10-11 13:45:56 -0600

    fixed region

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -2,32 +2,30 @@
 provider "aws" {
   profile = "default"
   shared_credentials_file = "/Users/venkatagarimella/.aws/credentials"
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 
 resource "aws_instance" "udacityT5P1R1" {
-ami = "ami-0947d2ba12ee1ff75"
+ami = "ami-03657b56516ab7912"
 instance_type= "t2.micro"
 tags = {
  Name= "udacity T2"
 }
 count= 4
 subnet_id = "subnet-0f4620252d2bb6a2a"
-
 }
 
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
 
 resource "aws_instance" "udacityT5P1R2" {
-ami = "ami-0947d2ba12ee1ff75"
+ami = "ami-03657b56516ab7912"
 instance_type = "m4.large"
 tags = {
  Name= "udacity M4"
 }
 count = 2
 subnet_id = "subnet-0f4620252d2bb6a2a"
-
 }
